341d4a553a7e8a2f0364002de3365eee186f3bad
brittanywang <35832643+brittanywang@users.noreply.github.com>
2018-06-15 11:09:39 -0700

    updated ALL files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import App from './App';
+import {YelpAPI} from './api';  
+import {weatherapi} from './weatherapi';  
+import { Router, browserHistory, Route, Link } from 'react-router';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+render((
+  <Router history={browserHistory}>
+    <Route exact path="/" component={App}/>
+    <Route exact path="/api" component={YelpAPI}/>
+    <Route exact path="/weatherapi" component={weatherapi}/>
+  </Router>
+), document.getElementById('root'))
